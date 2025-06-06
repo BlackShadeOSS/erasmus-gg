7ec1f5d77614e3199f10a704f60a998acc136bad
BlackShade <diaxsio10@gmail.com>
2025-06-06 19:13:26 +0200

    Fixed file placement and updated vocaba to mach

--- src/app/testing/vocaba/page.tsx
+++ src/app/testing/vocaba/page.tsx
@@ -1,16 +1,10 @@
 import Vocaba from "@/components/vocaba/vocaba";
 
 export default function VocabaPage() {
-  return (
-    <html lang="en">
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Vocaba</title>
-    </head>
-    <body>
-        <p>Vocaba test</p>
-        <Vocaba width={100} height={100}></Vocaba>
-    </body>
-    </html>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <p>Vocaba test</p>
+            <Vocaba width={100} height={100}></Vocaba>
+        </div>
+    );
 }
