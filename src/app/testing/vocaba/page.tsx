644a0f17f8a598d875acb0f1f6d3149619f5508b
DarekParodia <darekparodia@gmail.com>
2025-06-06 14:11:32 +0200

    vocaba folder structure

---
+++ src/app/testing/vocaba/page.tsx
@@ -0,0 +1,16 @@
+import Vocaba from "@/components/vocaba/vocaba";
+
+export default function VocabaPage() {
+  return (
+    <html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Vocaba</title>
+    </head>
+    <body>
+        <p>Vocaba test</p>
+        <Vocaba title="test"></Vocaba>
+    </body>
+    </html>
+  );
+}
