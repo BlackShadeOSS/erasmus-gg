55c96f9ed46b02741d0d1835d06663f536693fb8
laitdsgn <tomek08.konto@gmail.com>
2025-06-13 21:38:39 +0200

    created NavBar and improved structure

--- src/app/(main-page)/page.tsx
+++ src/app/(main-page)/page.tsx
@@ -1,15 +1,35 @@
+import NavBar from "@/components/NavBar";
 import { LineShadowText } from "@/components/ui/line-shadow-text";
 
 export default function Home() {
   return (
-    <main>
-      <div></div>
-      <h1 className="text-8xl w-full font-bold text-center my-10">
-        Ucz się
-        <LineShadowText className="mx-4 italic text-yellow-200 ">
-          efektywniej
-        </LineShadowText>
-      </h1>
-    </main>
+    <div className="relative">
+      <NavBar />
+
+      <main>
+        <section className="h-screen flex flex-col items-center justify-center">
+          <h1 className="text-7xl w-full font-bold text-center text-white">
+            Ucz się z <br />
+            <LineShadowText className="mx-2 italic">Vocenglish</LineShadowText>
+          </h1>
+        </section>
+
+        <section className="bg-gray-100 text-gray-900 p-10">
+          <div className="max-w-4xl mx-auto">
+            <h2 className="text-3xl font-semibold mb-4">
+              dalsza czesc strony etc etc.
+            </h2>
+            <p className="mb-4">
+              BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
+              BLA BLA BLA BLA BLA BLA BLA BLA BLA
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac
+              justo ut eros ornare malesuada.
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
