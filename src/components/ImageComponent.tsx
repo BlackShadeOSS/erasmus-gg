3f577b5e361ac5290f325c8a0004d31364a23aee
laitdsgn <tomek08.konto@gmail.com>
2025-07-08 16:22:12 +0200

    better navbar, footer, login adn register form, added example photos

--- src/components/ImageComponent.tsx
+++ src/components/ImageComponent.tsx
@@ -1,6 +1,13 @@
 import Image, { StaticImageData } from "next/image";
 import React from "react";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 interface ImageComponentProps {
   header: string;
   src: StaticImageData;
@@ -20,27 +27,32 @@ const ImageComponent: React.FC<ImageComponentProps> = ({
 }) => {
   return (
     <div className="p-4 md:p-8 flex flex-col gap-8 max-w-6xl mx-auto transition-transform duration-300 ease-in-out hover:scale-102">
-      <h2
-        className={`text-3xl font-light italic drop-shadow-lg drop-shadow-amber-200/20 ${
-          position === "right" ? "text-right" : "text-left"
-        }`}
-      >
-        {header}
-      </h2>
-
       <div
         className={`flex ${
           position === "right" ? "justify-end" : "justify-start"
         }`}
       >
         <div className="w-full md:w-4/5 lg:w-3/5">
-          <Image
-            src={src}
-            alt={alt}
-            width={width}
-            height={height}
-            className="object-cover w-full rounded-md shadow-2xl shadow-amber-200/30 "
-          />
+          <Card
+            className={`${
+              position === "right"
+                ? "shadow-[-10px_10px_32px_rgba(0,0,0,0.35)]"
+                : "shadow-[10px_10px_32px_rgba(0,0,0,0.35)]"
+            }`}
+          >
+            <CardHeader>
+              <h2 className="text-2xl italic">{header}</h2>
+            </CardHeader>
+            <CardContent>
+              <Image
+                src={src}
+                alt={alt}
+                width={width}
+                height={height}
+                className="object-cover w-full h-full rounded-md  shadow-md"
+              />
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
