8c42735602dc616a3d74fe7b7c57b83c2e38436b
Zuzek16 <zuzekzych@gmail.com>
2025-09-03 21:46:02 +0200

    connection to API

--- src/app/(games)/pamiec/page.tsx
+++ src/app/(games)/pamiec/page.tsx
@@ -2,11 +2,16 @@
 import Plansza from "@/components/pamiec/plansza";
 import { createRoot } from 'react-dom/client';
 import { ReactElement } from "react";
+import * as api from "@/app/(games)/pamiec/api-conn";
 import "./styles.css";
+import { log } from "node:console";
+import { importJWK } from "jose";
 
 //globals
 let planszaPojemnikNode:any;
 let planszaEl:any;
+
+
 // let listaSlowek: any;
 // let pobranoSlowka: boolean = false;
 // let zapytSkon: boolean = false;
@@ -35,8 +40,19 @@ let planszaEl:any;
     
 // }
  //TODO api not loaded use the default or smth
+ //z src/app/testing/user
+    async function fetcher(url: string, opts?: RequestInit) {
+        const res = await fetch(url, {
+            ...opts,
+            headers: { "Content-Type": "application/json",
+                
+             },
+             credentials: "include"
+        });
+        const json = await res.json();
+        return { ok: res.ok, json };
+    }
 
- 
 
 enum Trudnosc {
     latwa = 1,
@@ -55,19 +71,45 @@ const trudnosci: {
     
 }[] = [];
 
+//TODO seperate file
+function zapytanieFetch(method:string, url:string, cookie: boolean = true, data:any = null) {
+    if (cookie) {
+        
+        //TODO failsafe if used with document not loaded??
+        return fetch(url, {
+        method: method,
+        credentials: 'include',
+        cache: "no-cache",
+        headers: {
+            // 'Cookie': `auth-token=${authCookie}`,    
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: data ? JSON.stringify(data) : undefined
+    });
+    } else {
+        return fetch(url, {
+        method: method,
+        cache: "no-cache",
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: data ? JSON.stringify(data) : undefined
+    });
+    }
+    
+}
 
-// function zapytanieFetch(method:string, url:string, data:any = null) {
-//     return fetch(url, {
-//         method: method,
-//         cache: "no-cache",
-//         headers: {
-//             'Access-Control-Allow-Origin': '*',
-//             'Content-Type': 'application/json',
-//             'Accept': 'application/json'
-//         },
-//         body: data ? JSON.stringify(data) : undefined
-//     });
-// }
+// console.log(localStorage.getItem('auth-token'));
+
+async function pobierzSlowka(param:string = "") {
+    const url: string = "api/user/vocabulary" + param;
+    return zapytanieFetch("GET", url)
+    // return fetcher(url)
+}
 
 function zacznijGre(e: any) {
     e.preventDefault();
@@ -87,6 +129,7 @@ function rysujPlansze(trudnosc: number) {
     rysujxyKart(wysokoscPlanszy, szerokoscPlanszy);
     
 }
+
 
 /**
  * Rysuje 'karty' w prostokącie
@@ -127,20 +170,74 @@ function rysujxyKart(x: number, y: number) {
 // let planszaPojemnikNode:any = document.getElementById('planszaPojemnik');
 // let planszaEl:any = createRoot(planszaPojemnikNode);
 planszaPojemnikNode = document.getElementById('planszaPojemnik');
+
+
+
 planszaEl = createRoot(planszaPojemnikNode);
    
             planszaEl.render(<Plansza
             szerokosc={x}
             wysokosc={y}
             root={planszaEl}
+            pobranaListaSlowek={pobraneSlowka}
             />);
 //TODO wyświetl próby
             
     
 }
 
+const isClient = () => typeof window !== 'undefined';
+
+function getCookie(cname: string) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+} 
+let pobranoSlowka = false;
+let authCookie: string;
+let pobraneSlowka: {pol: string, ang: string}[] = [];
+
 export default function pamiec() {
+
+    //?is it better to pass this to the func or let it get it itself?
+    if (isClient()) {
+//TODO WAIT FOR THIS TO LOAD
+    if (!pobranoSlowka) {
+        
+    pobierzSlowka().then((res)=> {
+        return res.json();
+    })
+    .then((a)=>{
+        a.items.forEach((slowko: any) => {
+            console.log(slowko);
+            
+            pobraneSlowka.push({
+                pol: slowko.term_pl,
+                ang: slowko.term_en
+                
+            })            
+        });
+        
+    }).finally(()=>{
     
+    pobranoSlowka = true;
+        
+    }).catch((error)=>{
+        console.log("Error fetching :" + error);
+    })
+    }
+  }
+   
     for (let i = 1; i <= Trudnosc.ekspert; i++) {
 
         let nazwa = Trudnosc[i];
@@ -162,13 +259,7 @@ export default function pamiec() {
     const listaTrudnosci: ReactElement[] = trudnosci.map((trudnosc, count) => {
         return <option className="bg-transparent" key={count} value={trudnosc.key} >{trudnosc.nazwa}</option>//is this alright???
     });
-
-    //  zapytanieFetch("GET", "https://localhost:3000/api/user/vocabulary")
-    // .then(response => response.json())
-    // .then(voc => {console.dir(voc)})
-    // .catch(error => console.error('Error fetching users:', error));
-
-
+ 
     return (
        <div className="flex flex-col items-center justify-center min-h-screen p-10">
             <h1 className="text-4xl font-bold text-green-300 mb-10">Pamięć</h1>
@@ -177,31 +268,20 @@ export default function pamiec() {
 
                 <p className="flex flex-col items-center justify-center">Zaleca się rozgrywkę na komputerze.</p>
 
-                <form id="startForm" className="flex flex-col justify-center sm:hidden flex" method="post" onSubmit={zacznijGre}>
+                <form id="startForm" className="flex flex-col justify-center mb-10" method="post" onSubmit={zacznijGre}>
                     <h3 className="text-center mt-5">Wybierz trudność:</h3>
                     <select className="text-center max-w-fit m-auto bg-green-900 " name="trudnosc" id="trudnoscSelect">
                         {listaTrudnosci}
                     </select>
-                    <button className="mt-4 max-w-fit m-auto bg-green-200 border border-green-900 rounded-[6px] shadow-sm 
+                    <button disabled={pobranoSlowka} className="disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 mt-4 max-w-fit m-auto bg-green-200 border border-green-900 rounded-[6px] shadow-sm 
            box-border text-black text-[16px] font-bold
             p-3 px-4 hover:bg-transparent hover:text-green-200 hover:border-green-200
-           active:opacity-50" type="submit">start</button>
+           active:opacity-50
+           " type="submit"  >start</button>
                 </form>
 
                 <h3 className="text-green-200 text-center">Jak grać?</h3>
                 <p className="2xl:mx-80 sm:mx-36 mx-10 ">Kliknij kartę aby ją odsłonić. Odkryj drugą kartę, jeśli pasują (słówko po angielsku do słówka po polsku) karty znikają z pola. Jeśli nie pasują, zapamiętaj ich pozycję i ich zawartość. Karty zostaną ponownie zakryte i możesz próbować dalej. Gra kończy się gdy pole zostanie oczyszczone z kart lub gdy wszystkie próby zostaną wykorzystane.</p>
-
-                <form id="startForm" className="flex flex-col justify-center hidden sm:flex " method="post" onSubmit={zacznijGre}>
-                    <h3 className="text-center mt-5">Wybierz trudność:</h3>
-                    <select className="text-center max-w-fit m-auto bg-green-900 " name="trudnosc" id="trudnoscSelect">
-                        {listaTrudnosci}
-                    </select>
-                    <button className="mt-4 max-w-fit m-auto bg-green-200 border border-green-900 rounded-[6px] shadow-sm 
-           box-border text-black text-[16px] font-bold
-            p-3 px-4 hover:bg-transparent hover:text-green-200 hover:border-green-200
-           active:opacity-50" type="submit">start</button>
-                </form>
-
             </main>
             <div id="planszaPojemnik" className="w-screen"></div>
             <div id="wynikiPojemnik" className="flex flex-col items-center justify-center"></div>
